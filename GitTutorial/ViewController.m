d3a02e197063509a9b48b630b06b0ebee4bf68dd
cuongtvwru <cuongtv.wru@gmail.com>
2019-01-14 16:41:37 +0700

    commit to remote

--- GitTutorial/ViewController.m
+++ GitTutorial/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Hello World");
     NSLog(@"I'm TVC, I'm 21 year old");
+    NSLog(@"Hello, this is commit to remote");
 }
 
 
